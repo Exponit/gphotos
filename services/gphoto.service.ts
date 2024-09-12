eb545b36e952d3e3dc4cb520211a57f0000b302d
aryanmultani <aryan160401@gmail.com>
2024-09-12 09:37:39 +0530

    validate images

--- services/gphoto.service.ts
+++ services/gphoto.service.ts
@@ -5,12 +5,18 @@ import {  writeFileSync } from 'fs'
 export const getPhoto = async (galleryURL: string) => {
     let res: any = await fetch(galleryURL)
     res = await res.text()
-    
+    console.log('galleryURL',galleryURL);
+    // console.log('R.E.S-----',res);
     const pat = /https:\/\/lh3\.googleusercontent\.com\/pw\/AP1.*?-no/g;
     const urls = res.match(pat)
 
-    console.log('data', urls)
-    return urls
+    // Validate each URL
+    const validUrls = await Promise.all(urls.map(async (url) => {
+        const response = await fetch(url, { method: 'HEAD' });
+        return response.ok ? url : null;
+    }));
+
+    return validUrls.filter(Boolean);
 }
 
 export const setSize = (srcUrl: string, width?: number, height?: number) => {
